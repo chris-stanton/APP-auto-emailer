378d266404b9afdbfb912b41d9caae8c4f8adb20
Chris Stanton <cstanton0760@yahoo.com>
2017-05-02 23:53:38 -0500

    added notyf and sweatalerts2, sourced in and working

--- public/scripts/controllers/add.controller.js
+++ public/scripts/controllers/add.controller.js
@@ -3,12 +3,16 @@ myApp.controller('AddController', function() {
   // console.log('homeController running');
 
   var self = this;
+//must have variable for notyf
+  var notyf = new Notyf();
 
   self.message = 'angular Home sourced';
 
 //sends new company to DB
   self.addCompany = function(newCompany){
     console.log(newCompany);
+    notyf.confirm('works')
+    swal("flag Added To Database", "Success", "success");
   }//end of newCompany
 
 
