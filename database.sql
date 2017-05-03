6fa2f5bf70d1a6a1cba6c0bde21cbef14234a42e
Chris Stanton <cstanton0760@yahoo.com>
2017-05-02 23:37:23 -0500

    corrected sql doc

--- database.sql
+++ database.sql
@@ -1,5 +1,15 @@
 
--- CREATE DATABASE CALLED auto_emailer
+-- CREATE SQL DATABASE CALLED auto_emailer
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	displayName VARCHAR(100) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	photo VARCHAR(2000) NOT NULL
+);
+
+	INSERT INTO users (id, displayName, email, photo)
+	VALUES (1, 'yourName', 'yourEmailAddress@gmail.com', 'https://www.addressGoesHere.com/');
 
 CREATE TABLE companies (
 	id SERIAL PRIMARY KEY,
@@ -9,19 +19,9 @@ CREATE TABLE companies (
 	contactEmail VARCHAR(100) NOT NULL,
 	contactDate VARCHAR(80) NOT NULL,
 	note VARCHAR(5000),
-	active boolean DEFAULT true
+	active boolean DEFAULT true,
+	users_id integer REFERENCES users (id)
 );
 
-INSERT INTO companies (id, companyName, contactFirstName, contactLastName, contactEmail, contactDate, note, active)
-VALUES (1, 'Prime', 'Jon', 'Doe', 'example@gmail.com', '01/01/17', 'Notes display here!!!', 'true');
-
-CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
-	displayName VARCHAR(100) NOT NULL,
-	email VARCHAR(100) NOT NULL,
-	photo VARCHAR(2000) NOT NULL,
-	companies_id integer REFERENCES companies (id)
-);
-
-INSERT INTO users (id, displayName, email, photo, companies_id)
-VALUES(1, 'yourName', 'yourEmailAddress@gmail.com', 'https://www.addressGoesHere.com/', 1)
+	INSERT INTO companies (id, companyName, contactFirstName, contactLastName, contactEmail, contactDate, note, active, users_id)
+	VALUES (1, 'Prime', 'Jon', 'Doe', 'example@gmail.com', '01/01/17', 'Notes display here!!!', 'true', 1);
